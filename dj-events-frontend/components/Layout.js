dccf275ea805802fa704c42450229c447856de11
Helio Hiroshi <helio.fototerra@gmail.com>
2021-09-06 16:23:39 -0300

    jsconfig e showcase

--- dj-events-frontend/components/Layout.js
+++ dj-events-frontend/components/Layout.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
-import styles from '../styles/Layout.module.css';
+import Showcase from './Showcase';
+import styles from '@/styles/Layout.module.css';
 
 export default function Layout({
   title,
@@ -9,6 +11,8 @@ export default function Layout({
   description,
   children,
 }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -17,6 +21,7 @@ export default function Layout({
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer></Footer>
     </div>
